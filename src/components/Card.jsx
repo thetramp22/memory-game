67f8ae0b2f4c9b0cdbf2a8b30bbb8770781ee59d
thetramp22 <scottstarks.games@gmail.com>
2023-11-16 10:37:11 -0800

    Added randomize cards on click

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import '../styles/Card.css';
 import PropTypes from 'prop-types';
 
-const Card = ({ name, image }) => {
+const Card = ({ name, image, randomizeCards }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    if (isClicked) {
+      //endgame
+    } else {
+      //addscore
+      setIsClicked(true);
+    }
+    randomizeCards();
+  };
+
   return (
-    <div className="card">
+    <div className="card" onClick={handleClick}>
       <img className="card-img" src={image} alt={name} />
       <h2 className="card-title">{name}</h2>
     </div>
@@ -13,6 +26,7 @@ const Card = ({ name, image }) => {
 Card.propTypes = {
   name: PropTypes.string,
   image: PropTypes.string,
+  randomizeCards: PropTypes.func,
 };
 
 export default Card;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,42 @@
 import '../styles/Game.css';
 import Card from './Card';
 import cardInfo from '../assets/data/cardInfo';
+import { useState } from 'react';
 
 const Game = () => {
+  const [positions, setPositions] = useState(
+    cardInfo.map((value, index) => index)
+  );
+
+  const shuffleArray = (array) => {
+    let shuffledArray = array.slice(0);
+    for (let i = 0; i < shuffledArray.length; i++) {
+      const j = Math.floor(Math.random() * shuffledArray.length);
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
+  const randomizeCards = () => {
+    setPositions(shuffleArray(positions));
+  };
+
   return (
     <>
       <div className="main">
         <div className="card-grid">
-          {cardInfo.map((card) => {
-            return <Card key={card.name} name={card.name} image={card.image} />;
+          {positions.map((position) => {
+            return (
+              <Card
+                key={cardInfo[position].name}
+                name={cardInfo[position].name}
+                image={cardInfo[position].image}
+                randomizeCards={randomizeCards}
+              />
+            );
           })}
         </div>
       </div>
